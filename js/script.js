473a580f9e3ed6c7a031f6d08f7087e4b200b531
BrnFbrian <103138343+BrnFbrian@users.noreply.github.com>
2023-05-30 11:35:26 -0300

    Update script.js
    teste em alterar o acesso a pasta de notas para ver se funciona no site

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const switcher = document.querySelector('.switcher');
 const keysSection = document.querySelector('.piano__keys');
 
 const playNote = (note) => {
-    const audio = new Audio(`../notes/${note}.wav`);
+    const audio = new Audio(`./notes/${note}.wav`);
     audio.play();
 }
 
